da1fe63fca074ed1ed7ad49207568b599b707640
Lalit Ponnala <lalit.p@gmail.com>
2023-06-22 15:21:25 -0400

    commit

--- example.py
+++ example.py
@@ -8,6 +8,8 @@ import glee
 
 data_file = "data_glee_k1-k6.csv"
 fitplots_file = "glee_k1-k6_fitplots.png"
+stnpvals_file = "glee_k1-k6_stnpval.png"
+out_file = "glee_k1-k6_results.csv"
 nA = 2
 nB = 2
 fit_type = "cubic"
@@ -21,10 +23,13 @@ assert not D.isna().any().any(), "data: missing values found"
 assert D.shape[0] >= 5, "data: not enough rows"
 assert D.shape[1] == (1+nA+nB), "data: incorrect number of columns"
 
+proteins = D.iloc[:,0]
 dat = {'A': D.iloc[:,1:(1+nA)], 'B': D.iloc[:,(1+nA):(1+nA+nB)]}
 model = glee.fit_model(dat, fit_type=fit_type, num_pts=num_pts, num_std=num_std)
 glee.model_fit_plots(model, file=fitplots_file)
-glee.calc_stn_pval(dat, model, num_iter)
+stn_pval = glee.calc_stn_pval(dat, model, num_iter)
+stn_pval_plots = glee.stn_pval_plots(stn_pval, out_file=stnpvals_file)
+tab = glee.diff_exp_table(stn_pval, proteins, num_digits)
 
 # list final p-values in the same order as D
 # TODO
--- glee.py
+++ glee.py
@@ -4,6 +4,14 @@ import numpy as np
 import statsmodels.formula.api as smf
 import matplotlib.pyplot as plt
 
+def diff_exp_table(stn_pval, Prot, num_digits):
+    None
+
+
+def stn_pval_plots(stn_pval, out_file):
+    None
+
+
 def get_p_vals(model_stn, iter, model_stn_dist):
     None
 
@@ -31,6 +39,24 @@ def calc_stn_pval(dat, model, num_iter):
     p_value = get_p_vals(model_stn, num_iter,stn_distrib)
     assert np.isfinite(p_value).all(), "p_value: contains non-finite values"
     return {'model_stn': model_stn, 'p_value': p_value}
+
+
+def model_fit_plots(model, file=None):
+    _,axs = plt.subplots(nrows=2, ncols=2, figsize=(8,8))
+    for i,k in enumerate(model):
+        mod = model[k]
+        df_pts = pd.concat([mod['xbar_showpts'], mod['stdev_showpts']], axis=1)
+        df_pts.plot(kind='scatter', x='xbar_log', y='stdev_log', grid=True, ax=axs[i,0], alpha=0.3)
+        df_fit = pd.concat([mod['xbar_showfit'], mod['stdev_showfit']], axis=1)
+        df_fit.set_index('xbar_log').squeeze().plot(kind='line', xlabel='log(mean)', ylabel='log(stdev)', title=f"Condition {k}", grid=True, ax=axs[i,0])
+        axs[i,0].text(x=df_fit['xbar_log'].min(), y=df_fit['stdev_log'].max(), s=f"adj.Rsq = {round(mod['adjRsq'],3)}")
+        df_pts['xbar_log'].sort_values().to_frame().assign(protein=list(range(1,df_pts.shape[0]+1))).plot(kind='scatter', x='protein', y='xbar_log', ylabel='signal level (mean)', title=f"Condition {k}", grid=True, ax=axs[i,1])
+    plt.tight_layout()
+    if file is None:
+        plt.show()
+    else:
+        plt.savefig(file)
+    plt.close()
 
 
 def fit_model(dat, fit_type, num_pts=20, num_std=5):
@@ -65,24 +91,6 @@ def fit_model(dat, fit_type, num_pts=20, num_std=5):
         # collect into a dict to be returned
         model[k] = {'model': reg, 'adjRsq': adjRsq, 'coeffs': coeffs, 'xbar_showfit': xbar_showfit, 'stdev_showfit': stdev_showfit, 'xbar_showpts': xbar_showpts, 'stdev_showpts': stdev_showpts, 'xbar': xbar, 'stdev': stdev}
     return model
-
-def model_fit_plots(model, file=None):
-    _,axs = plt.subplots(nrows=2, ncols=2, figsize=(8,8))
-    for i,k in enumerate(model):
-        mod = model[k]
-        df_pts = pd.concat([mod['xbar_showpts'], mod['stdev_showpts']], axis=1)
-        df_pts.plot(kind='scatter', x='xbar_log', y='stdev_log', grid=True, ax=axs[i,0], alpha=0.3)
-        df_fit = pd.concat([mod['xbar_showfit'], mod['stdev_showfit']], axis=1)
-        df_fit.set_index('xbar_log').squeeze().plot(kind='line', xlabel='log(mean)', ylabel='log(stdev)', title=f"Condition {k}", grid=True, ax=axs[i,0])
-        axs[i,0].text(x=df_fit['xbar_log'].min(), y=df_fit['stdev_log'].max(), s=f"adj.Rsq = {round(mod['adjRsq'],3)}")
-        df_pts['xbar_log'].sort_values().to_frame().assign(protein=list(range(1,df_pts.shape[0]+1))).plot(kind='scatter', x='protein', y='xbar_log', ylabel='signal level (mean)', title=f"Condition {k}", grid=True, ax=axs[i,1])
-    plt.tight_layout()
-    if file is None:
-        plt.show()
-    else:
-        plt.savefig(file)
-    plt.close()
-
 
 # ---- OLD CODE ----
 
